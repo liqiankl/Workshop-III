3d521a5cfc3f0326775216ad67f9cdc0bad111c7
Beulah Khumlo <khumlobeulah@gmail.com>
2021-01-12 01:07:12 +0530

    BottomTab

---
+++ src/components/Background.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import{StyleSheet, View} from 'react-native';
+import Svg, {Path} from 'react-native-svg';
+
+
+
+const Background =()=>{
+    r
+    return(
+        <View style={style}>
+        <View style={style.viewAbove}>
+          <Svg height ='50%'
+          ><Path 
+          fill="#9a009"
+          d="M0,256L48,261.3C96,267,192,277,288,245.3C384,213,480,139,576,101.3C672,64,768,64,864,101.3C960,139,1056,213,1152,218.7C1248,224,1344,160,1392,128L1440,96L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"/></Svg>
+        </View>
+        </View>
+);};
+
+export default Background;
+
+const styles = StyleSheet.creat({
+viewAbove:{
+    backgroundColor:'#9a009',
+    height: 350,
+}
+});
