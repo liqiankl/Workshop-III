ba60aa2b255d11344a31bc902e13c6cd063dd246
Beulah Khumlo <khumlobeulah@gmail.com>
2021-01-12 18:52:06 +0530

    svg added

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,12 +1,48 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {
+  
+    StyleSheet,
+
+    View,
+    Text,
+    StatusBar,
+    Dimensions,
+
+  } from 'react-native';
+
+import Background from '../components/Background';
 
 
-const Home=()=>{return(<>
-    <View><Text>Screen Home</Text></View>
+
+const Home=()=>{return(
+   <>
+      <StatusBar barStyle="dark-content" />
+      <View style={styles.container}>
+        <Background style={styles.svg} />
+        <Text>Hello</Text>
+      
+      </View>
    </>);
 
 };
+
+const styles = StyleSheet.create({
+    scrollView: {
+      flex: 1,
+    },
+    container: {
+      flex: 1,
+      position: 'relative',
+      backgroundColor: 'white',
+    },
+    svg: {
+      position: 'absolute',
+      width: Dimensions.get('window').width,
+    },
+ 
+   
+    
+  });
 
 export default Home;
 
